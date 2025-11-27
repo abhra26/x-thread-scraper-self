932272b506801f6f556105b2ef6a8e8c51027df4
Aaryan Gajanan Bhujang <162456484+aaryanbhujang@users.noreply.github.com>
2025-11-27 14:50:25 +0530

    Delete src/xscraper directory

--- src/xscraper/core/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Core module containing the main scraping engine components.
-"""
-
-from .scraper import ThreadScraper
-from .authenticator import XAuthenticator
-from .rate_limiter import AdaptiveRateLimiter
-
-__all__ = ["ThreadScraper", "XAuthenticator", "AdaptiveRateLimiter"]
--- src/xscraper/utils/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Utility functions and helpers for the X Thread Scraper.
-"""
-
-from .helpers import validate_thread_url, extract_thread_id
-from .logger import setup_logger, get_logger
-
-__all__ = ["validate_thread_url", "extract_thread_id", "setup_logger", "get_logger"]
